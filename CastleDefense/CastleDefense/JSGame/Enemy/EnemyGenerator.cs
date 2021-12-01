805a34189c6f0f9458483d5fe28194cc0a2c4948
You can call me J <jsy724724@gmail.com>
2021-11-30 22:08:29 -0500

    Update EnemyGenerator.cs

--- CastleDefense/CastleDefense/JSGame/Enemy/EnemyGenerator.cs
+++ CastleDefense/CastleDefense/JSGame/Enemy/EnemyGenerator.cs
@@ -18,7 +18,32 @@ namespace CastleDefense
 
         public EnemyGenerator(Game game) : base(game)
         {
-            
+            Game1 g = (Game1)game;
+
+            /* Enemy */
+            Texture2D texEnemy1 = g.Content.Load<Texture2D>("images/Enemy/01Wolf");
+            Enemy e1 = new Enemy(game, texEnemy1);
+
+
+            Texture2D texEnemy2 = g.Content.Load<Texture2D>("images/Enemy/02RedBat");
+            Enemy e2 = new Enemy(game, texEnemy2);
+
+            Enemies = new List<Enemy>();
+
+            Enemies.Add(e2);
+            Enemies.Add(e1);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            MouseState ms = Mouse.GetState();
+
+            if (ms.LeftButton == ButtonState.Pressed)
+            {
+                Dispose();
+            }
+
+            base.Update(gameTime);
         }
     }
 }
