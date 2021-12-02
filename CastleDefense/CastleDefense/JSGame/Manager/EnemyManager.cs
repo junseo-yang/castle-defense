7f8b098bee08c0cd670fa6010fd67114f26b1526
You can call me J <jsy724724@gmail.com>
2021-12-01 21:01:41 -0500

    Create EnemyManager

--- CastleDefense/CastleDefense/JSGame/Manager/EnemyManager.cs
+++ CastleDefense/CastleDefense/JSGame/Manager/EnemyManager.cs
@@ -8,42 +8,31 @@ using Microsoft.Xna.Framework.Input;
 
 namespace CastleDefense
 {
-    public class EnemyGenerator : GameComponent
+    public class EnemyManager : JSDrawableGameComponent
     {
         public List<Enemy> Enemies { get; set; }
-        public int Level { get; set; }
-        public int NumberOfEnemies { get; set; }
 
         private Random r;
 
-        public EnemyGenerator(Game game) : base(game)
+        public EnemyManager(Game game) : base(game)
         {
             Game1 g = (Game1)game;
+
+            Enemies = new List<Enemy>();
 
             /* Enemy */
             Texture2D texEnemy1 = g.Content.Load<Texture2D>("images/Enemy/01Wolf");
             Enemy e1 = new Enemy(game, texEnemy1);
-
+            Enemies.Add(e1);
 
             Texture2D texEnemy2 = g.Content.Load<Texture2D>("images/Enemy/02RedBat");
             Enemy e2 = new Enemy(game, texEnemy2);
-
-            Enemies = new List<Enemy>();
-
             Enemies.Add(e2);
-            Enemies.Add(e1);
         }
 
-        public override void Update(GameTime gameTime)
+        public void CreateEnemies()
         {
-            MouseState ms = Mouse.GetState();
 
-            if (ms.LeftButton == ButtonState.Pressed)
-            {
-                Dispose();
-            }
-
-            base.Update(gameTime);
         }
     }
 }
