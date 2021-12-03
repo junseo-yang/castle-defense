2d0362668b31023bb37526fce79b6ad75e7cae79
You can call me J <jsy724724@gmail.com>
2021-12-02 21:58:17 -0500

    Create EntityManager.cs

---
+++ CastleDefense/CastleDefense/JSGame/EntityManager.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CastleDefense
+{
+    static class EntityManager
+    {
+        static List<Entity> entities = new List<Entity>();
+        static List<Enemy> enemies = new List<Enemy>();
+        static List<Arrow> arrows = new List<Arrow>();
+
+        static bool isUpdating;
+        static List<Entity> addedEntities = new List<Entity>();
+
+        public static int Count { get { return entities.Count; } }
+
+        public static void Add(Entity entity)
+        {
+            if (!isUpdating)
+                AddEntity(entity);
+            else
+                addedEntities.Add(entity);
+        }
+
+        private static void AddEntity(Entity entity)
+        {
+            entities.Add(entity);
+            if (entity is Arrow)
+                arrows.Add(entity as Arrow);
+            else if (entity is Enemy)
+                enemies.Add(entity as Enemy);
+        }
+
+        public static void Update()
+        {
+            isUpdating = true;
+            // HandleCollisions();
+
+            foreach (var entity in entities)
+                entity.Update();
+
+            isUpdating = false;
+
+            foreach (var entity in addedEntities)
+                AddEntity(entity);
+
+            addedEntities.Clear();
+
+            entities = entities.Where(x => !x.IsExpired).ToList();
+            arrows = arrows.Where(x => !x.IsExpired).ToList();
+            enemies = enemies.Where(x => !x.IsExpired).ToList();
+        }
+
+
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var entity in entities)
+                entity.Draw(spriteBatch);
+        }
+    }
+}
