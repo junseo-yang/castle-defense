606375430869bca1f76e29c18ef3769565a9c467
You can call me J <jsy724724@gmail.com>
2021-12-02 21:58:23 -0500

    Create Sound.cs

---
+++ CastleDefense/CastleDefense/JSGame/Sound.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+namespace CastleDefense
+{
+    static class Sound
+    {
+        public static Song Music { get; private set; }
+
+		public static void Load(ContentManager content)
+		{
+			Music = content.Load<Song>("songs/Song1");
+		}
+	}
+}
