0ff7d8463080481eeae8283d7d42d4549fced793
You can call me J <jsy724724@gmail.com>
2021-12-05 16:51:05 -0500

    Create GameScene.cs

---
+++ CastleDefense/CastleDefense/JSGame/GameScene.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CastleDefense
+{
+    public abstract class GameScene : DrawableGameComponent
+    {
+        public SpriteBatch spriteBatch;
+        public Texture2D texture2D;
+
+        public List<GameComponent> SceneComponents { get; set; }
+
+        public virtual void show()
+        {
+            this.Visible = true;
+            this.Enabled = true;
+        }
+        public virtual void hide()
+        {
+            this.Visible = false;
+            this.Enabled = false;
+        }
+
+        public GameScene(Game game) : base(game)
+        {
+            SceneComponents = new List<GameComponent>();
+            hide();
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            DrawableGameComponent comp = null;
+            foreach (GameComponent item in SceneComponents)
+            {
+                if (item is DrawableGameComponent)
+                {
+                    comp = (DrawableGameComponent)item;
+                    if (comp.Visible)
+                    {
+                        comp.Draw(gameTime);
+                    }
+                }
+            }
+
+            base.Draw(gameTime);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (GameComponent item in SceneComponents)
+            {
+                if (item.Enabled)
+                {
+                    item.Update(gameTime);
+                }
+            }
+
+            base.Update(gameTime);
+        }
+    }
+}
