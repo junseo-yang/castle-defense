bf95aea6f88fa1eebb8d47c2dac2cef045821409
You can call me J <jsy724724@gmail.com>
2021-12-04 23:09:11 -0500

    Update EnemySpawner.cs

--- CastleDefense/CastleDefense/JSGame/EnemySpawner.cs
+++ CastleDefense/CastleDefense/JSGame/EnemySpawner.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,24 +11,34 @@ namespace CastleDefense
         static float inverseSpawnChance = 90;
         static float inverseBlackHoleChance = 600;
 
-
 		public static void Update()
 		{
-			if (!PlayerShip.Instance.IsDead && EntityManager.Count < 200)
+			if (!Archer.Instance.IsDead && EntityManager.Count < 200)
 			{
-				if (rand.Next((int)inverseSpawnChance) == 0)
-					EntityManager.Add(Enemy.CreateSeeker(GetSpawnPosition()));
-
-				if (rand.Next((int)inverseSpawnChance) == 0)
-					EntityManager.Add(Enemy.CreateWanderer(GetSpawnPosition()));
-
-				if (EntityManager.BlackHoleCount < 2 && rand.Next((int)inverseBlackHoleChance) == 0)
-					EntityManager.Add(new BlackHole(GetSpawnPosition()));
-			}
+                if (rand.Next((int)inverseSpawnChance) == 0)
+                    EntityManager.Add(Enemy.CreateRandomEnemy(GetSpawnPosition()));
+            }
 
 			// slowly increase the spawn rate as time progresses
 			if (inverseSpawnChance > 30)
 				inverseSpawnChance -= 0.005f;
+		}
+
+		private static Vector2 GetSpawnPosition()
+		{
+			Vector2 pos = new Vector2(0, rand.Next(300, 500));
+			//do
+			//{
+			//	pos = new Vector2(0 , rand.Next(0, (int)Shared.stage.Y));
+			//}
+			//while (Vector2.DistanceSquared(pos, Archer.Instance.Position) < 250 * 250);
+
+			return pos;
+		}
+
+		public static void Reset()
+		{
+			inverseSpawnChance = 90;
 		}
 	}
 }
