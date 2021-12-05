d25824cce75b40afb1786ed8b33413d62e80cc55
You can call me J <jsy724724@gmail.com>
2021-12-04 23:09:19 -0500

    Update Extensions.cs

--- CastleDefense/CastleDefense/JSGame/Extensions.cs
+++ CastleDefense/CastleDefense/JSGame/Extensions.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
 
-namespace CastleDefense.JSGame
+namespace CastleDefense
 {
     static class Extensions
     {
