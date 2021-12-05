fbc61319e5a64a549a5cffc9058e7bb37cccdf6a
You can call me J <jsy724724@gmail.com>
2021-12-04 23:09:46 -0500

    Create MathUtil.cs

---
+++ CastleDefense/CastleDefense/JSGame/MathUtil.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CastleDefense
+{
+    static class MathUtil
+    {
+        public static Vector2 FromPolar(float angle, float magnitude)
+        {
+            return magnitude * new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+        }
+    }
+}
