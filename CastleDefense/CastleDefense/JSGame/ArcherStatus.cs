744a0951e8dec588dd339cbbad0382fa1c1958ec
You can call me J <jsy724724@gmail.com>
2021-12-05 23:47:16 -0500

    Create ArcherStatus.cs
    Player Name
    File Name
    Level System

---
+++ CastleDefense/CastleDefense/JSGame/ArcherStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CastleDefense
+{
+    static class ArcherStatus
+    {
+        public static bool IsGameOver = false;
+
+        static ArcherStatus()
+        {
+            Reset();
+        }
+
+        public static void Reset()
+        {
+            IsGameOver = false;
+        }
+    }
+}
