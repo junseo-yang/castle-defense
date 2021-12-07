e0ae1d2ca66a687f32a173cf9fb6a35b9336f4e9
You can call me J <jsy724724@gmail.com>
2021-12-06 21:24:50 -0500

    Update StartScene.cs

--- CastleDefense/CastleDefense/JSGame/StartScene.cs
+++ CastleDefense/CastleDefense/JSGame/StartScene.cs
@@ -13,19 +13,40 @@ namespace CastleDefense
         private MenuComponent menu;
         public MenuComponent Menu { get => menu; set => menu = value; }
 
-        string[] menuItems = { "Start game", "Help", "High Score", "Credit", "Quit" };
+        string[] menuItems = { "Start game", "Load Game", "Help", "High Score", "Credit", "Quit" };
+
+        SpriteFont regularFont;
+        SpriteFont hilightFont;
+        SpriteFont titleFont;
 
         public StartScene(Game game) : base(game)
         {
             Game1 g = (Game1)game;
 
             spriteBatch = g._spriteBatch;
-            SpriteFont regularFont = g.Content.Load<SpriteFont>("fonts/regularFont");
-            SpriteFont hilightFont = g.Content.Load<SpriteFont>("fonts/hilightFont");
+
+            regularFont = Art.RegularFont;
+            hilightFont = Art.HilightFont;
+            titleFont = Art.TitleFont;
 
             menu = new MenuComponent(g, spriteBatch, regularFont, hilightFont, menuItems);
             this.SceneComponents.Add(menu);
+        }
 
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            // spriteBatch.DrawString(titleFont, "Castle Defense", Shared.stage / 2, Color.Black, 0f, );
+            DrawRightAlignedString(titleFont, "Castle Defense", 50f);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawRightAlignedString(SpriteFont spriteFont, string text, float y)
+        {
+            var textWidth = spriteFont.MeasureString(text).X;
+            spriteBatch.DrawString(spriteFont, text, new Vector2((Shared.stage.X - textWidth) / 2, y), Color.Black);
         }
     }
 }
