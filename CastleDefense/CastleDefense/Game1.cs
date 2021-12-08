7aec3e81139729b938c2f39de61f7c4e9d9a92d8
You can call me J <jsy724724@gmail.com>
2021-12-07 23:59:40 -0500

    Update Game1.cs
    1. Declare Constants
    2. HighScoreScene
    3. Modify FileName
    4. Update SetPlayerName
    5. Add LoadGame

--- CastleDefense/CastleDefense/Game1.cs
+++ CastleDefense/CastleDefense/Game1.cs
@@ -12,17 +12,28 @@ namespace CastleDefense
 {
     public class Game1 : Game
     {
+        public const int START_GAME = 0;
+        public const int LOAD_GAME = 1;
+        public const int HELP_SCENE = 2;
+        public const int HIGH_SCORE_SCENE = 3;
+        public const int CREDIT_SCENE = 4;
+        public const int QUIT = 5;
+
         private GraphicsDeviceManager _graphics;
         public SpriteBatch _spriteBatch;
 
         /* Scene */
         private StartScene startScene;
-        private HelpScene helpScene;
         public ActionScene actionScene;
-        public const int QUIT = 5;
+        private HelpScene helpScene;
+        public HighScoreScene highScoreScene;
+        public CreditScene creditScene;
+
 
         public static string PlayerName { get; set; }
-        public static string FileName { get; set; }
+        public static string FileName { get; set; } = "CastleDefenseSave.txt";
+
+        int selectedIndex = 0;
 
         public Game1()
         {
@@ -41,10 +52,18 @@ namespace CastleDefense
             Shared.stage = new Vector2(_graphics.PreferredBackBufferWidth,
                 _graphics.PreferredBackBufferHeight);
 
+            //do
+            //{
+            //    SetDirectory();
+            //} while (string.IsNullOrEmpty(FileName));
+
+            CreateSaveFile();
+
             do
             {
-                SetDirectory();
-            } while (string.IsNullOrEmpty(PlayerName) || string.IsNullOrEmpty(FileName));
+                SetPlayerName();
+            } while (string.IsNullOrEmpty(PlayerName));
+
 
             base.Initialize();
         }
@@ -76,6 +95,9 @@ namespace CastleDefense
             actionScene = new ActionScene(this);
             this.Components.Add(actionScene);
 
+            highScoreScene = new HighScoreScene(this);
+            this.Components.Add(highScoreScene);
+
             startScene.show();
         }
 
@@ -93,29 +115,41 @@ namespace CastleDefense
 
             Input.Update();
 
-            int selectedIndex = 0;
             KeyboardState ks = Keyboard.GetState();
             if (startScene.Enabled)
             {
                 selectedIndex = startScene.Menu.SelectedIndex;
-                if (selectedIndex == 0 && ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
+                if (selectedIndex == START_GAME && ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
                 {
                     startScene.hide();
                     actionScene.RestartGame();
                     actionScene.show();
                 }
-                if (selectedIndex == 1 && ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
+                if (selectedIndex == LOAD_GAME && ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
                 {
-                    //startScene.hide();
-                    //actionScene.RestartGame();
-                    //actionScene.show();
+                    try
+                    {
+                        LoadGame();
+                        startScene.hide();
+                        actionScene.show();
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Error: " + ex.Message, "Castle Defense");
+                    }
+
                 }
-                else if (selectedIndex == 2 && ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
+                else if (selectedIndex == HELP_SCENE && ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
                 {
                     startScene.hide();
                     helpScene.show();
                 }
-                else if (selectedIndex == 3 && ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
+                else if (selectedIndex == HIGH_SCORE_SCENE && ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
+                {
+                    startScene.hide();
+                    highScoreScene.show();
+                }
+                else if (selectedIndex == CREDIT_SCENE && ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
                 {
                     startScene.hide();
                     helpScene.show();
@@ -126,7 +160,7 @@ namespace CastleDefense
                 }
             }
 
-            if (helpScene.Enabled || actionScene.Enabled)
+            if (helpScene.Enabled || actionScene.Enabled || highScoreScene.Enabled)
             {
                 if (ks.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
                 {
@@ -148,40 +182,41 @@ namespace CastleDefense
             base.Draw(gameTime);
         }
 
-        public void SetDirectory()
+        public void CreateSaveFile()
         {
             try
             {
-                System.Windows.Forms.MessageBox.Show("Before we start this game, you need to specify a location to create a game data save file to save scores.", "Catle Defense");
+                //System.Windows.Forms.MessageBox.Show("Before we start this game, you need to specify a location to create a game data save file to save scores.", "Catle Defense");
 
-                using (var fbd = new FolderBrowserDialog())
+                //using (var fbd = new FolderBrowserDialog())
+                //{
+                //    DialogResult result = fbd.ShowDialog();
+
+                //    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                //    {
+                //        FileName = fbd.SelectedPath + "\\CastleDefenseSave.txt";
+
+                //        if (!File.Exists(FileName))
+                //        {
+                //            File.Create(FileName).Dispose();
+                //            System.Windows.Forms.MessageBox.Show("CastleDefenseSave.txt is created under selected location.", "Castle Defense");
+                //        }
+                //        else
+                //        {
+                //            System.Windows.Forms.MessageBox.Show("CastleDefenseSave.txt is already created under selected location.", "Castle Defense");
+                //        }
+                //    }
+                //}
+
+                if (!File.Exists(FileName))
                 {
-                    DialogResult result = fbd.ShowDialog();
-
-                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
-                    {
-                        FileName = fbd.SelectedPath + "\\CastleDefenseSave.txt";
-
-                        if (!File.Exists(FileName))
-                        {
-                            File.Create(FileName).Dispose();
-                            System.Windows.Forms.MessageBox.Show("CastleDefenseSave.txt is created under selected location.", "Castle Defense");
-                        }
-                        else
-                        {
-                            System.Windows.Forms.MessageBox.Show("CastleDefenseSave.txt is already created under selected location.", "Castle Defense");
-                        }
-                    }
+                    File.Create(FileName).Dispose();
+                    // System.Windows.Forms.MessageBox.Show("CastleDefenseSave.txt is created under selected location.", "Castle Defense");
                 }
-
-                if (String.IsNullOrEmpty(FileName))
-                {
-                    System.Windows.Forms.MessageBox.Show("Location path is empty", "Castle Defense");
-                }
-                else
-                {
-                    SetPlayerName();
-                }
+                //else
+                //{
+                //    System.Windows.Forms.MessageBox.Show("CastleDefenseSave.txt is already created under selected location.", "Castle Defense");
+                //}
             }
             catch (Exception ex)
             {
@@ -191,12 +226,46 @@ namespace CastleDefense
 
         private void SetPlayerName()
         {
+            InputBox inputBox = new InputBox("Player Name", "You need to enter a player name to play.");
+            PlayerName = inputBox.GetName();
+
+            string[] records;
+            using (StreamReader reader = new StreamReader(Game1.FileName))
+            {
+                records = reader.ReadToEnd().Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var item in records)
+            {
+                if (item.StartsWith(PlayerName))
+                {
+                    PlayerName = null;
+                    System.Windows.Forms.MessageBox.Show("The Player Name already exists.");
+                    return;
+                }
+            }
+
             using (StreamWriter writer = new StreamWriter(FileName, append: true))
             {
-                InputBox inputBox = new InputBox("Player Name", "You need to enter a player name to play.");
-                PlayerName = inputBox.GetName();
+                writer.WriteLine(Game1.PlayerName + "\t" + ActionScene.Level + "\t" + ActionScene.Score);
+            }
+        }
 
-                writer.Write(PlayerName + "\t");
+        private void LoadGame()
+        {
+            using (StreamReader reader = new StreamReader(FileName))
+            {
+                // InputBox inputBox = new InputBox("Player Name", "You need to enter a player name to play.");
+                do
+                {
+                    string[] record = reader.ReadLine().Split("\t", StringSplitOptions.RemoveEmptyEntries);
+                    if (record[0].StartsWith(PlayerName))
+                    {
+                        ActionScene.Level = int.Parse(record[1]);
+                        ActionScene.Score = int.Parse(record[2]);
+                        break;
+                    }
+                } while (reader.EndOfStream);
             }
         }
     }
