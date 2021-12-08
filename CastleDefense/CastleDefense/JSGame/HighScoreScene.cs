534567db9a12efaca73a092c990bc8b5dd374b23
You can call me J <jsy724724@gmail.com>
2021-12-08 00:01:48 -0500

    Create HighScoreScene.cs

---
+++ CastleDefense/CastleDefense/JSGame/HighScoreScene.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CastleDefense
+{
+    public class HighScoreScene : GameScene
+    {
+        SpriteFont regularFont;
+        SpriteFont hilightFont;
+        SpriteFont titleFont;
+        SpriteFont scoreFont;
+
+        List<string[]> scores;
+
+        public HighScoreScene(Game game) : base(game)
+        {
+            Game1 g = (Game1)game;
+            spriteBatch = g._spriteBatch;
+
+            regularFont = Art.RegularFont;
+            hilightFont = Art.HilightFont;
+            titleFont = Art.TitleFont;
+            scoreFont = Art.ScoreFont;
+
+            scores = new List<string[]>();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Vector2 tempPos = new Vector2(Shared.stage.X / 2, 280f);
+
+            spriteBatch.Begin();
+            // spriteBatch.DrawString(titleFont, "Castle Defense", Shared.stage / 2, Color.Black, 0f, );
+            DrawRightAlignedString(titleFont, "Castle Defense", 50f);
+            DrawRightAlignedString(regularFont, "Top 5 High Score", 200f);
+            DrawRightAlignedString(scoreFont, String.Format("{0,-30} {1,-15} {2,-15}", "Player Name", "Level", "Score"), 250f);
+
+            int i = 0;
+            foreach (var score in scores)
+            {
+                DrawRightAlignedString(scoreFont, String.Format("{0,-30} {1,-15} {2,-15}", score[0], score[1], score[2]), tempPos.Y);
+                tempPos.Y += hilightFont.LineSpacing;
+                i++;
+                if (i == 5)
+                {
+                    break;
+                }
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            LoadScores();
+
+            base.Update(gameTime);
+        }
+
+        private void DrawRightAlignedString(SpriteFont spriteFont, string text, float y)
+        {
+            var textWidth = spriteFont.MeasureString(text).X;
+            spriteBatch.DrawString(spriteFont, text, new Vector2((Shared.stage.X - textWidth) / 2, y), Color.Black);
+        }
+
+        private void LoadScores()
+        {
+            using (StreamReader reader = new StreamReader(Game1.FileName))
+            {
+                // InputBox inputBox = new InputBox("Player Name", "You need to enter a player name to play.");
+                List<string[]> records = new List<string[]>();
+                while (!reader.EndOfStream)
+                {
+                    string[] record = reader.ReadLine().Split(new char[] { '\t', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                    records.Add(record);
+                }
+
+                if (records.Count > 1)
+                {
+                    records.Sort((delegate (string[] x, string[] y)
+                    {
+                        return int.Parse(y[2]) - int.Parse(x[2]);
+                    }));
+                }
+
+                scores = records;
+            }
+        }
+    }
+}
