02c0ef0ab9c7109768b520faa2e50ca8425ee765
You can call me J <jsy724724@gmail.com>
2021-12-08 09:06:37 -0500

    Create Font.cs

---
+++ CastleDefense/CastleDefense/JSGame/Font.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CastleDefense
+{
+    class Font
+    {
+        public static SpriteFont RegularFont { get; private set; }
+        public static SpriteFont HilightFont { get; private set; }
+        public static SpriteFont TitleFont { get; private set; }
+        public static SpriteFont ScoreFont { get; private set; }
+
+        public static void Load(ContentManager content)
+        {
+            RegularFont = content.Load<SpriteFont>("fonts/regularFont");
+            HilightFont = content.Load<SpriteFont>("fonts/hilightFont");
+            TitleFont = content.Load<SpriteFont>("fonts/titleFont");
+            ScoreFont = content.Load<SpriteFont>("fonts/scoreFont");
+        }
+    }
+}
