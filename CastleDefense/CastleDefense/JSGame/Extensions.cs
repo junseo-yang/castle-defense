c157799a6ee449ba125a6d4da364b672b224b708
You can call me J <jsy724724@gmail.com>
2021-12-09 16:37:43 -0500

    Update Extensions.cs

--- CastleDefense/CastleDefense/JSGame/Extensions.cs
+++ CastleDefense/CastleDefense/JSGame/Extensions.cs
@@ -8,12 +8,6 @@ namespace CastleDefense
 {
     static class Extensions
     {
-		//public static void DrawLine(this SpriteBatch spriteBatch, Vector2 start, Vector2 end, Color color, float thickness = 2f)
-		//{
-		//	Vector2 delta = end - start;
-		//	spriteBatch.Draw(Art.Pixel, start, null, color, delta.ToAngle(), new Vector2(0, 0.5f), new Vector2(delta.Length(), thickness), SpriteEffects.None, 0f);
-		//}
-
 		public static float ToAngle(this Vector2 vector)
 		{
 			return (float)Math.Atan2(vector.Y, vector.X);
